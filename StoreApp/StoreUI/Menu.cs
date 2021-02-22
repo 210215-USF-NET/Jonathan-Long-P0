9a86386b35328204b2f9e4f6f4cfa9aa122c1952
Jack Long <jacklong34@gmail.com>
2021-02-22 17:10:44 -0500

    Added functionality to main menu

--- StoreApp/StoreUI/Menu.cs
+++ StoreApp/StoreUI/Menu.cs
@@ -5,8 +5,33 @@ namespace StoreUI
     {
         public void Start()
         {
+            bool menuRun = true;
+            do
+            {
+            Console.WriteLine();
             Console.WriteLine("Welcome to the Ski Store!");
             Console.WriteLine("-------------------------");
+            Console.WriteLine("Please Select an Option:");
+            Console.WriteLine("[0] - Customer Options");
+            Console.WriteLine("[1] - Manager Options");
+            Console.WriteLine("Select Option: ");
+            string option = Console.ReadLine();
+            switch(option)
+            {
+                case "0":
+                    //Go To Customer Menu
+                    break;
+                case "1":
+                    //Go to Manager Menu
+                    break;
+                case "2":
+                    menuRun = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid menu option. Please try again!");
+                    break;
+            }
+            } while (menuRun);
         }
     }
 }
